8a3a2920ae8c93f3d10d9dcc176269db9e155baf
Aditya Prawira <adityaprawira96@gmail.com>
2021-03-04 16:17:35 +1100

    Added Linear and Advanced Linear Search methods in Array application

--- array_app/array_app.cpp
+++ array_app/array_app.cpp
@@ -41,7 +41,8 @@ void Insert(struct Array *arr, int index, int x){
 };
 
 // Deleted element at the specified index
-int Delete(struct Array *arr, int index){
+int Delete(struct Array *arr, int index)
+{
     int x = 0;
     int i;
     if (index >= 0 && index < arr->length)
@@ -57,6 +58,44 @@ int Delete(struct Array *arr, int index){
     };
     return 0;
 };
+// Linear Search: the function will take a value and return index value when the element that being given is contained in the Array
+// Don't need arr as an address because we don't modify any of its data.
+int LinearSearch(struct Array arr, int key){
+    int i;
+    for(i = 0; i< arr.length; i++){
+        if(key == arr.A[i]){
+            printf("%d is found at index %d", key, i);
+            return i;
+        }
+    }
+    printf("%d is not found\n", key);
+    return -1;
+};
+
+// Linear Search with transposition
+// In this case arr need to be an address because we need to modify data in Array.
+void swap(int *x, int *y)
+{
+    int temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
+};
+int AdvancedLinearSearch(struct Array *arr, int key){
+    int i;
+    for(i = 0; i< arr->length; i++){
+        if(key == arr->A[i]){
+            // Example: 2 3 7 5 6 10, want to find 6
+            // 6 is found and swap is value will its preceding value which is 5
+            // Then, 2 3 7 6 5 10
+            swap(&arr->A[i], &arr->A[i - 1]);
+            printf("%d is found at index %d", key, i);
+            return i;
+        }
+    }
+    printf("%d is not found\n", key);
+    return -1;
+};
 int main()
 {
     // array  in this case is arr
@@ -66,6 +105,8 @@ int main()
     Append(&arr, 10); // 2 3 4 5 6 10
     Insert(&arr, 3, 7); // 2 3 4 7 5 6 10
     printf("Element of %d is being deleted from Array\n", Delete(&arr, 2)); // Current array 2 3 7 5 6 10
+    printf("\nLinear Search: Element of %d in array\n", LinearSearch(arr, 6));
+    printf("\nAdvanced Linear Search: Element of %d in array\n", AdvancedLinearSearch(&arr, 6)); // Current array 2 3 7 6 5 10 
     Display(arr);
     return 0;
 };
