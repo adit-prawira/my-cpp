dcc88eeadc526a7cabcc498e9fea3716ab3c32c4
Aditya Prawira <adityaprawira96@gmail.com>
2021-03-04 15:53:46 +1100

    Added Delete method for Array application

--- arr/insert.cpp
+++ arr/insert.cpp
@@ -29,7 +29,7 @@ void Insert(struct Array *arr, int index, int x){
     if (index >= 0 && index <= arr->length)
     {
         // Loop to insert new elements in the specified index and then shift the other elements to the
-        // index allocation.
+        // correct index allocation by 1 index to the right.
         for (i = arr->length; i > index; i--){
             arr->A[i] = arr->A[i - 1];
         };
@@ -40,6 +40,23 @@ void Insert(struct Array *arr, int index, int x){
     };
 };
 
+// Deleted element at the specified index
+int Delete(struct Array *arr, int index){
+    int x = 0;
+    int i;
+    if (index >= 0 && index < arr->length)
+    {
+        x = arr->A[index];
+        // Loop to shift all non targeted elements in array to the correct index allocation by 1 index to the left.
+        for (i = index; i < arr->length - 1; i++){
+            arr->A[i] = arr->A[i + 1];
+        }
+        arr->length--;
+        // return value that being deleted at the specified index early when it has been found.
+        return x;
+    };
+    return 0;
+};
 int main()
 {
     // array  in this case is arr
@@ -47,7 +64,8 @@ int main()
     // length 5 which is the current length and there are 5 spaces left to reach maximum elemnts which is 10
     struct Array arr = {{2, 3, 4, 5, 6}, 10, 5};
     Append(&arr, 10); // 2 3 4 5 6 10
-    Insert(&arr, 9, 7); // 2 3 4 7 5 6 10
+    Insert(&arr, 3, 7); // 2 3 4 7 5 6 10
+    printf("Element of %d is being deleted from Array\n", Delete(&arr, 2)); // Current array 2 3 7 5 6 10
     Display(arr);
     return 0;
 };
