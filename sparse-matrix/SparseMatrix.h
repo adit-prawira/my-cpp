03d5e3431f8e536430d3dda48c3312ab20f4a07f
Aditya Prawira <adityaprawira96@gmail.com>
2021-03-07 20:29:38 +1100

    Added method to sum 2 SparseMatrix

--- sparse-matrix/SparseMatrix.h
+++ sparse-matrix/SparseMatrix.h
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <string>
 #include "Element.h"
+
 using namespace std;
 class SparseMatrix{
     private:
@@ -22,16 +23,21 @@ class SparseMatrix{
         ~SparseMatrix(){
             delete [] elements;
         }
-        friend istream & operator >>(istream &is, SparseMatrix &s);
+        SparseMatrix operator+(SparseMatrix &s);
+        friend istream &operator>>(istream &is, SparseMatrix &s);
         friend ostream &operator<<(ostream &os, SparseMatrix &s);
 };
-istream & operator >>(istream &is, SparseMatrix &s){
+
+// cin >> matrix, where matrix is type of Sparse Matrix.
+istream &operator>>(istream &is, SparseMatrix &s)
+{
     cout << "Enter non-zero elements:"<< endl;
     for (int i = 0; i< s.num; i++){
         cin >> s.elements[i].row >> s.elements[i].col >> s.elements[i].val;
     }
     return is;
 };
+// cout << matrix, where matrix is type of Sparse Matrix.
 ostream &operator<<(ostream &os, SparseMatrix &s){
     int k = 0;
     for (int i = 0; i < s.m; i++)
@@ -46,4 +52,38 @@ ostream &operator<<(ostream &os, SparseMatrix &s){
         cout << endl;
     }
     return os;
+}
+
+SparseMatrix SparseMatrix::operator+(SparseMatrix &s){
+    int i, j, k;
+    if(m != s.m || n != s.n){
+        return SparseMatrix(s.m, s.n, 0);
+    }
+    SparseMatrix *sum = new SparseMatrix(m, n, num + s.num);
+    i = j = k = 0;
+    while(i < num && j < s.num){
+        if(elements[i].row < s.elements[j].row){
+            sum->elements[k++]= elements[i++];
+        }else if (elements[i].row > s.elements[j].row){
+            sum->elements[k++] = elements[j++];
+        }else{
+            if(elements[i].col < s.elements[j].col){
+                sum->elements[k++]= elements[i++];
+            }else if (elements[i].col > s.elements[j].col){
+                sum->elements[k++] = elements[j++];
+            }else{
+                sum->elements[k] = elements[i];
+                sum->elements[k++].val = elements[i++].val + s.elements[j++].val;
+            }
+        }
+    }
+    // whatever value of i as long as it is less than num
+    for (; i < num; i++){
+        sum->elements[k++] = elements[i];
+    }
+    for (; j < s.num; j++){
+        sum->elements[k++] = s.elements[j];
+    }
+    sum->num = k;
+    return *sum;
 }
--- sparse-matrix/main.cpp
+++ sparse-matrix/main.cpp
@@ -4,7 +4,12 @@ using namespace std;
 
 int main(){
     SparseMatrix matrix1(5, 5, 5);
+    SparseMatrix matrix2(5, 5, 5);
     cin >> matrix1;
-    cout << matrix1;
+    cin >> matrix2;
+    SparseMatrix sum = matrix1 + matrix2;
+    cout << "First Matrix" << endl << matrix1;
+    cout << "Second Matrix" << endl << matrix2;
+    cout << "Sum Matrix" << endl << sum;
     return 0;
 }
