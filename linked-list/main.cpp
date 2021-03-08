2345586ac9c5866434596f12bd3296d215e0625d
Aditya Prawira <adityaprawira96@gmail.com>
2021-03-08 18:20:44 +1100

    Added linked list application

---
+++ linked-list/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <array>
+using namespace std;
+// linked list is initially NULL
+struct Node{
+    int data;
+    struct Node *next;
+} * first = NULL;
+
+void create(int A[], int n){
+    int i;
+    struct Node *temp, *last;
+
+    //Zeroth element is created
+    first = (struct Node *)malloc(sizeof(struct Node));
+    first->data = A[0];
+    first->next = NULL;
+    last = first;
+
+    // Creating the next elements
+    for (i = 1; i < n; i++){
+        temp = (struct Node *)malloc(sizeof(struct Node));
+        temp->data = A[i];
+        temp->next = NULL;
+        // set next value of the the latest last which is 0th Node .next = 1th Node value
+        // then change the last value as 1th Node, where the 1th Node.next = NULL.
+        // Then set next value of the the latest last which is 1th Node .next = 2nd Node value
+        // then change the last value as 2nd Node, where the 2nd Node.next = NULL, and so on.
+        last->next = temp;
+        last = temp;
+    }
+}
+
+void display(struct Node *node){
+    while(node != NULL){
+        cout << node->data << " ";
+
+        // move tot he next node
+        node = node->next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int A[] = {3, 5, 7, 10, 15};
+    int n = sizeof(A) / sizeof(A[0]);
+    create(A, n);
+    display(first);
+    return 0;
+}
